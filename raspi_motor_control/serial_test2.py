1f7b0d3c7764ce8a39d49a1ddb01a80e3808e401
choiyooung <42990331+choiyooung@users.noreply.github.com>
2018-11-14 20:58:49 +0900

    sleep 0.1
    sleep 0.1
    speed 5
    angle 2

--- raspi_motor_control/serial_test2.py
+++ raspi_motor_control/serial_test2.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
 
 Xposition = "0"
 Yposition = "1"
@@ -10,16 +10,15 @@ time.sleep(2)
 while 1:
     X = Xposition.encode('utf-8')
     Y = Yposition.encode('utf-8')
-    //c=c.encode('utf-8')
+    #c=c.encode('utf-8')
     print(X,Y)
     
     arduino.write(X);
     arduino.write(Y);
     
     print("Hi python")
-        #print(arduino.readline())
-    time.sleep(0.01)
+    time.sleep(0.1)
     i = i + 1;
-    if i==360:
+    if i==50:
         break
 
