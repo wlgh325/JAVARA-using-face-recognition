d57e783a80ce27c60f1c77caff760435c3b34f54
choiyooung <42990331+choiyooung@users.noreply.github.com>
2018-11-14 20:59:39 +0900

    angle2 speed5

--- raspi_motor_control/serial_XYstep/serial_XYstep.ino
+++ raspi_motor_control/serial_XYstep/serial_XYstep.ino
@@ -1,8 +1,8 @@
 #include <Stepper.h>
 
 const int STEPS = 2048;
-char X = 0;
-char Y = 1;
+char X = '0';
+char Y = '1';
 char serialChar = 0;
 Stepper stepper(STEPS, 8,9,10,11);
 Stepper stepper2(STEPS, 4,5,6,7);
@@ -10,21 +10,28 @@ String str="";
 
 void setup()
 {
-  stepper.setSpeed(15);
-  stepper2.setSpeed(15);
+  stepper.setSpeed(5);
+  stepper2.setSpeed(5);
   Serial.begin(9600);
 }
 
 void loop()
 {
+  int angle=0;
+
   if(Serial.available()) {
     serialChar = Serial.read();
-   if(serialChar == X)
-  {
-    stepper.step(1);
-  }
-  else if(serialChar == Y)
-  {
-    stepper2.step(1);
+    if(serialChar == X){
+      angle = map(2,0,360,0,2048);
+      stepper.step(angle);
+      Serial.write("X\n");
+    }
+    else if(serialChar == Y){
+      angle = map(2,0,360,0,2048);
+      stepper2.step(angle);
+      Serial.write("Y\n");
+    }
   }
 }
+
+
