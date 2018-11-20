3a2314f885423a0182a88f8d272ccf21a27e9894
wlgh325 <wlgh325@naver.com>
2018-11-20 14:39:15 +0900

    -각도 수정

--- raspi_motor_control/serial_XYstep/serial_XYstep.ino
+++ raspi_motor_control/serial_XYstep/serial_XYstep.ino
@@ -1,37 +1,60 @@
-#include <Stepper.h>
+#include <StepperMulti.h>
 
 const int STEPS = 2048;
-char X = '0';
-char Y = '1';
-char serialChar = 0;
-Stepper stepper(STEPS, 8,9,10,11);
-Stepper stepper2(STEPS, 4,5,6,7);
+StepperMulti stepper(STEPS, 8,9,10,11);
+StepperMulti stepper2(STEPS, 4,5,6,7);
 String str="";
 
 void setup()
 {
-  stepper.setSpeed(5);
-  stepper2.setSpeed(5);
+  stepper.setSpeed(10);
+  stepper2.setSpeed(10);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  int angle=0;
-
   if(Serial.available()) {
-    serialChar = Serial.read();
-    if(serialChar == X){
-      angle = map(2,0,360,0,2048);
-      stepper.step(angle);
-      Serial.write("X\n");
+    char temp[10];    //serial로 부터 받을 buffer
+    byte leng = Serial.readBytes(temp,4); //byte형태로 받아서 temp저장, 크기 반환
+    int flag; //flag =0 일때 +각도 flag=1일때 -각도
+    int i;
+
+    if(temp[1] == "-"){
+        i=1;
+        flag=1;
     }
-    else if(serialChar == Y){
-      angle = map(2,0,360,0,2048);
-      stepper2.step(angle);
-      Serial.write("Y\n");
+    else{
+      i=0;
+      flag=0;
     }
+
+    //String으로 변환
+    for(int i=0; i<leng; i++){
+      str += temp[i];
+    }
+
+    //string을 int형으로 변환
+    int angle = str.toInt();
+
+    //step수에 맞게 변환
+    angle = map(angle,0,360,0,2048);
+
+    if(flag == 0){
+      //angle만큼 정회전
+      stepper.setStep(angle);
+      stepper2.setStep(angle);
+    }
+    else if(flag == 1){
+      stepper.setStep(-angle);
+      stepper2.setStep(-angle);
+    }
+
   }
+
+  stepper.moveStep();
+  stepper2.moveStep();
+  
+  //다시 값 전달 받기 전에 str 초기화
+  str="";
 }
-
-
